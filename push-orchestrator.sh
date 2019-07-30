0bb63db070cf2ab51818757d1211d2dea1c54dd3
Patrick Quinn-Graham <make-contact@pftqg.com>
2019-07-30 23:31:55 +1000

    Remove previous (non-GCP) encoder

--- push-orchestrator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t thepatrick/meetupsencoder-orchestrator -f Dockerfile.orchestrator .
-sudo docker push thepatrick/meeetupsencoder-orchestrator:latest
--- run-worker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-sudo docker build -t thepatrick/meetupsencoder-worker -f Dockerfile.worker .
-sudo docker run --rm -it --env REDIS_HOST=10.244.67.2 -v `pwd`/storage:`pwd`/storage thepatrick/meetupsencoder-worker
