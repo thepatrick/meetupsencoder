eb76750a48a0d0ce9def5d44e7fc74a6f0d0f5b7
Patrick Quinn-Graham <make-contact@pftqg.com>
2019-07-31 08:25:44 +1000

    Include the storage functions in worker-gcp

---
+++ worker-gcp/src/storage/downloadFromGoogleCloudStorage.ts
@@ -0,0 +1,19 @@
+import { Storage } from '@google-cloud/storage';
+import { sendOrchestratorLogger } from '../api/sendOrchestratorLog';
+
+export const downloadFromGoogleCloudStorage = async (
+  storage: Storage,
+  logger: sendOrchestratorLogger,
+  src: string,
+  dest: string,
+): Promise<void> => {
+  const srcURL = new URL(src);
+
+  const bucket = storage.bucket(srcURL.host);
+  const file = bucket.file(srcURL.pathname);
+
+  logger('info', 'Downloading', { src, dest });
+  await file.download({
+    destination: dest,
+  });
+};
